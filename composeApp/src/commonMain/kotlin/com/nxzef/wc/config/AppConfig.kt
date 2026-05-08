b22e3b67172b3d3ae00b4aa5b0d79673495e5bef
nxzef <165759706+nxzef@users.noreply.github.com>
2026-05-08 19:46:55 +0530

    Update AppConfig to enable production mode and set the production base URL

--- composeApp/src/commonMain/kotlin/com/nxzef/wc/config/AppConfig.kt
+++ composeApp/src/commonMain/kotlin/com/nxzef/wc/config/AppConfig.kt
@@ -1,7 +1,7 @@
 package com.nxzef.wc.config
 
 object AppConfig {
-    const val IS_PRODUCTION = false
+    const val IS_PRODUCTION = true
 
     val BASE_URL get() = if (IS_PRODUCTION) {
         "https://wc-server.up.railway.app"
